0b5c303cf09cc453a49763f058377ef307ba4069
Henry Woo <hcwoo2@illinois.edu>
2020-06-04 19:46:19 -0500

    Starting to see successful training! Still need to try setting the testing phase to eval mode and algorithm is unfinished. Also, try testing with non-normalized feature vectors

--- prototype/generator.py
+++ prototype/generator.py
@@ -14,90 +14,93 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-class Generator(nn.Module):
-    def __init__(self, width=12, deconv=False):
-        super(Generator, self).__init__()
-        # Input size: [batch, 3, 32, 32]
-        # Output size: [batch, 3, 32, 32]
-        nfms = width
-        if deconv:
-            self.decoder = nn.Sequential(
-                # [batch, width*8, 2, 2]
-                nn.ConvTranspose2d(512, nfms * 8, 4, stride=2, padding=1),
-                nn.BatchNorm2d(nfms * 8),
-                nn.LeakyReLU(0.2, inplace=True),
-                # [batch, width*4, 4, 4]
-                nn.ConvTranspose2d(nfms*8, 256, 4, stride=2, padding=1),
-                nn.BatchNorm2d(256),
-                nn.LeakyReLU(0.2, inplace=True),
-                # [batch, width*2, 8, 8]
-                nn.ConvTranspose2d(256, 256, 4, stride=2, padding=1),
-                nn.BatchNorm2d(256),
-                nn.LeakyReLU(0.2, inplace=True),
-                # [batch, width*1, 16, 16]
-                nn.ConvTranspose2d(256, 256, 4, stride=2, padding=1),
-                nn.BatchNorm2d(256),
-                nn.Conv2d(256, 256, kernel_size=3, stride=1,
-                          padding=0),  # [batch, width*1, 14, 14]
-            )
-        else:
-            self.decoder = nn.Sequential(
-                nn.Upsample(size=(2, 2), mode='nearest'),
-                nn.Conv2d(512, nfms*8, kernel_size=2, stride=1, padding=1),
-                nn.LeakyReLU(0.2, inplace=True),
-                # state size.
-                nn.Upsample(size=(4, 4), mode='nearest'),
-                nn.Conv2d(nfms*8, 256, kernel_size=3, stride=1, padding=1),
-                nn.LeakyReLU(0.2, inplace=True),
-                # state size. (ngf*8) x 4 x 4
-                nn.Upsample(size=(8, 8), mode='nearest'),
-                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-                nn.LeakyReLU(0.2, inplace=True),
-                # state size. (ngf*4) x 8 x 8
-                nn.Upsample(size=(14, 14), mode='nearest'),
-                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            )
-        self.output_act = nn.Tanh()
-
-    def forward(self, x):
-        x = x.view(-1, 512, 1, 1)
-        decoded = self.decoder(x)
-        decoded = self.output_act(decoded)
-        return decoded
-
-
-def generator(deconv=False):
-    return Generator(width=32, deconv=deconv)
-
 # class Generator(nn.Module):
-#     ''' DCGAN Generator implementation '''
-
 #     def __init__(self, width=12, deconv=False):
 #         super(Generator, self).__init__()
-
-#         self.linear = nn.Linear(512, 128 * 4 * 4)
-
-#         self.decoder = nn.Sequential(
-#             nn.BatchNorm2d(128),
-#             nn.Upsample(scale_factor=2),
-#             nn.Conv2d(128, 128, 3, stride=1, padding=1),
-#             nn.BatchNorm2d(128, 0.8),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Upsample(scale_factor=2),
-#             nn.Conv2d(128, 64, 3, stride=1, padding=1),
-#             nn.BatchNorm2d(64, 0.8),
-#             nn.LeakyReLU(0.2, inplace=True),
-#             nn.Conv2d(64, 256, 3, stride=1, padding=1)
-#         )
+#         # Input size: [batch, 3, 32, 32]
+#         # Output size: [batch, 3, 32, 32]
+#         nfms = width
+#         if deconv:
+#             self.decoder = nn.Sequential(
+#                 # [batch, width*8, 2, 2]
+#                 nn.ConvTranspose2d(512, nfms * 8, 4, stride=2, padding=1),
+#                 nn.BatchNorm2d(nfms * 8),
+#                 nn.LeakyReLU(0.2, inplace=True),
+#                 # [batch, width*4, 4, 4]
+#                 nn.ConvTranspose2d(nfms*8, 256, 4, stride=2, padding=1),
+#                 nn.BatchNorm2d(256),
+#                 nn.LeakyReLU(0.2, inplace=True),
+#                 # [batch, width*2, 8, 8]
+#                 nn.ConvTranspose2d(256, 256, 4, stride=2, padding=1),
+#                 nn.BatchNorm2d(256),
+#                 nn.LeakyReLU(0.2, inplace=True),
+#                 # [batch, width*1, 16, 16]
+#                 nn.ConvTranspose2d(256, 256, 4, stride=2, padding=1),
+#                 nn.BatchNorm2d(256),
+#                 nn.Conv2d(256, 256, kernel_size=3, stride=1,
+#                           padding=0),  # [batch, width*1, 14, 14]
+#             )
+#         else:
+#             self.decoder = nn.Sequential(
+#                 nn.Upsample(size=(2, 2), mode='nearest'),
+#                 nn.Conv2d(512, nfms*8, kernel_size=2, stride=1, padding=1),
+#                 nn.LeakyReLU(0.2, inplace=True),
+#                 # state size.
+#                 nn.Upsample(size=(4, 4), mode='nearest'),
+#                 nn.Conv2d(nfms*8, 256, kernel_size=3, stride=1, padding=1),
+#                 nn.LeakyReLU(0.2, inplace=True),
+#                 # state size. (ngf*8) x 4 x 4
+#                 nn.Upsample(size=(8, 8), mode='nearest'),
+#                 nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+#                 nn.LeakyReLU(0.2, inplace=True),
+#                 # state size. (ngf*4) x 8 x 8
+#                 nn.Upsample(size=(14, 14), mode='nearest'),
+#                 nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+#             )
 #         self.output_act = nn.Tanh()
 
 #     def forward(self, x):
-#         x = self.linear(x)
-#         x = x.view(-1, 128, 4, 4)
+#         x = x.view(-1, 512, 1, 1)
 #         decoded = self.decoder(x)
 #         decoded = self.output_act(decoded)
 #         return decoded
 
 
 # def generator(deconv=False):
-#     return Generator()
+#     return Generator(width=32, deconv=deconv)
+
+
+class Generator(nn.Module):
+    def __init__(self, width=12, deconv=False):
+        super(Generator, self).__init__()
+
+        nfms = width
+        self.decoder = nn.Sequential(
+            nn.Upsample(size=(2, 2), mode='nearest'),
+            nn.Conv2d(2, nfms * 8, kernel_size=2, stride=1, padding=1),
+            nn.BatchNorm2d(nfms * 8, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size.
+            nn.Upsample(size=(4, 4), mode='nearest'),
+            nn.Conv2d(nfms * 8, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*8) x 4 x 4
+            nn.Upsample(size=(8, 8), mode='nearest'),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ngf*4) x 8 x 8
+            nn.Upsample(size=(14, 14), mode='nearest'),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = x.view(-1, 2, 16, 16)
+        decoded = self.decoder(x)
+        return decoded
+
+
+def generator(deconv=False):
+    return Generator()
